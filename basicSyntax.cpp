bfcf79e68e4c63f85a03ce5eff04206054110510
Facundo Becerra <facundo.g.becerra@gmail.com>
2023-11-24 09:16:47 +0100

    C++ start

---
+++ basicSyntax.cpp
@@ -0,0 +1,12 @@
+// My first program in C++
+
+#include <iostream>
+
+using namespace std; // With that you can not use std::
+
+int main()
+{
+    // std::
+    cout << "Hello World!";
+    return 0;
+};
